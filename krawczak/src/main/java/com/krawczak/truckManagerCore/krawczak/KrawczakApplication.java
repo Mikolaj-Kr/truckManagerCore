0fd60024427906515def434eeb1815826d6903e6
Mikolaj-Kr <mikolak25@gmail.com>
2021-01-31 19:32:10 +0100

    initial commit

---
+++ krawczak/src/main/java/com/krawczak/truckManagerCore/krawczak/KrawczakApplication.java
@@ -0,0 +1,13 @@
+package com.krawczak.truckManagerCore.krawczak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrawczakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrawczakApplication.class, args);
+	}
+
+}
---
+++ krawczak/src/test/java/com/krawczak/truckManagerCore/krawczak/KrawczakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krawczak.truckManagerCore.krawczak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrawczakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
